b15bcb656faec6ef1667204e3319d95f2fc18255
Guy Oliver <guyoliver03@gmail.com>
2024-02-20 13:01:22 -0500

    computer understands my arrays

--- front/js/script.js
+++ front/js/script.js
@@ -7,7 +7,13 @@ fetch(url)
     .then(data => {
         displayItems(data)
     })
-    
-function displayItems (sofas) {
-    const section = document.getElementById ('items')
+
+function displayItems(sofas) {
+    console.log(sofas);
+
+    const section = document.getElementById('items')
+    for (let i = 0; i < sofas.length; i++) {
+        const sofa = sofas [i]; 
+        section.innerHTML += ''
+    }
 }
