fd07976bdb23da51a301d224d3e7e3fe370ca7d6
Guy Oliver <guyoliver03@gmail.com>
2024-02-20 15:04:25 -0500

    got cards on homepage

--- front/js/script.js
+++ front/js/script.js
@@ -7,13 +7,23 @@ fetch(url)
     .then(data => {
         displayItems(data)
     })
+    .catch(err => console.log(err))
 
 function displayItems(sofas) {
     console.log(sofas);
 
     const section = document.getElementById('items')
     for (let i = 0; i < sofas.length; i++) {
-        const sofa = sofas [i]; 
-        section.innerHTML += ''
+        const sofa = sofas[i];
+        console.log(sofa.name)
+        // TODO finish adding html from homepage as a template to string
+        // let text = `Welcome ${firstName}, ${lastName}!`;
+        section.innerHTML += ` <a href="./product.html?id=${sofa._id}">
+        <article>
+          <img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">
+          <h3 class="productName">${sofa.name}</h3>
+          <p class="productDescription">${sofa.description}</p>
+        </article>
+      </a> `;
     }
 }
