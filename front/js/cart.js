f3d0f4c53ccbb2d47b66ae0b06d356dbf8f98176
Guy Oliver <guyoliver03@gmail.com>
2024-03-09 14:30:31 -0800

    added carItems to page

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,4 +1,22 @@
-console.log('hey'); 
+console.log('hey');
+const cartElement = document.getElementById('cart__items');
+const totalQuantityELement = document.getElementById('totalQuantity');
+const currentTotalPrice = document.getElementById('totalPrice');
+const cart = JSON.parse(localStorage.getItem('cart') || "[]");
+const cartProducts = [];
+
+for (let i = 0; i < cart.length; i++) {
+    const cartItem = cart[i];
+
+    fetch(`http://localhost:3000/api/products/${cartItems.prodcutId}`)
+        .then(data => {
+            return data.json();
+        })
+        .then(product => {
+            displayProducts(product, cartItem);
+        })
+
+}
 // TODO GET cart from localstorage
 // TODO loop over the cart
 // TODO FOR each cartItem get the product info from the back-end using the productID
