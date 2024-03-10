af7b2c9a4e827d2a1c6e52f53e889d57834a8782
Guy Oliver <OlivertwstTech@gmail.com>
2024-03-09 17:23:13 -0800

    completed todo's

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,20 @@
-console.log('hey'); 
-// TODO GET cart from localstorage
-// TODO loop over the cart
-// TODO FOR each cartItem get the product info from the back-end using the productID
-// TODO INSERT cartItem in page
+const cartElement = document.getElementById('cart__items');
+const totalQuantityElement = document.getElementById('totalQuantity');
+const currentTotalPrice = document.getElementById('totalPrice');
+const cart = JSON.parse(localStorage.getItem('cart') || "[]");
+const cartProducts = [];
+
+insertCartItems();
+
+async function insertCartItems() {
+    for (cartItem of cart) {
+        console.log(cartItem);
+        const productId = cartItem.productId;
+        const data = await fetch(`http://localhost:3000/api/products/${productId}`);
+        const product = await data.json();
+        console.log(product);
+        // TODO INSERT cartItem in page
+        // TODO update total quantity and total price on page 
+
+    }
+}
